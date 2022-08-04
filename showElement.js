315b15a5ed1b9963956caf22bd59748442b0e817
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-08-04 15:08:29 +0200

    Update showElement.js

--- showElement.js
+++ showElement.js
@@ -7,39 +7,70 @@ window.onload = function () {
         document.getElementById("divEmptyList").classList.remove("hidden");
     }
 
-
-    function showElements(el, datas) {
-
-        for (let j = 0; j < datas.length; j++) {
-            let elClone = el.cloneNode(true);
-
-            let lstElData = elClone.querySelectorAll("[data-prop]");
-
-            for (let i = 0; i < lstElData.length; i++) {
-
-                let currentProp = lstElData[i].getAttribute("data-prop");
-
-                if (datas[j].hasOwnProperty(currentProp)) {
-
-                    let whereInsert = lstElData[i].getAttribute("data-src");
-
-                    switch (whereInsert) {
-                        case "text": lstElData[i].textContent = datas[j][currentProp];
-                            break;
-                        case "name": lstElData[i].setAttribute("name", datas[j][currentProp]);
-                            break;
-                    }
-                    
-                }
-                else {
-                    alert(currentProp + " n'est pas renseignée.");
-                }
-            };
-
-            el.parentNode.append(elClone);
-            elClone.classList.remove("hidden");
-        };
-
-    }
 };
 
+function showElements(el, datas) {
+
+    if (datas.length) {
+        for (let j = 0; j < datas.length; j++) {
+            let data = datas[j];
+            showValues(el, data);
+        };
+    }
+    else {
+        showValues(el, datas);
+    }
+
+}
+
+function showValues(el, data) {
+
+    let elClone = el.cloneNode(true);
+
+    let lstElData = elClone.querySelectorAll("[data-prop]");
+
+    for (let i = 0; i < lstElData.length; i++) {
+
+        let currentProp = lstElData[i].getAttribute("data-prop");
+
+        if (data.hasOwnProperty(currentProp)) {
+
+            let whereInsert = lstElData[i].getAttribute("data-src");
+
+            switch (whereInsert) {
+                case "text": lstElData[i].textContent = data[currentProp];
+                    break;
+                case "name": lstElData[i].setAttribute("name", data[currentProp]);
+                    break;
+            }
+
+        }
+        else {
+            alert(currentProp + " n'est pas renseignée.");
+        }
+    };
+
+    el.parentNode.append(elClone);
+    elClone.classList.remove("hidden");
+}
+
+
+function getFetch(url, fonction) {
+
+    fetch("https://localhost:44302/" + url, { method: 'GET' })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            fonction(data)
+        })
+}
+
+
+function showDetails(elt) {
+    let idCat = elt.getAttribute('name');
+    getFetch("Home/catDetails/" + idCat, function (data) {
+        let elt = document.getElementById("divDetailsCat");
+        showElements(elt,data);
+    })
+}
