0d1851bd68c08b531c08dafba03c051ce125869f
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-08-08 13:16:51 +0200

    Update showElement.js

--- showElement.js
+++ showElement.js
@@ -1,14 +1,10 @@
-window.onload = function () {
-    if (Array.isArray(model) && model.length != 0) {
-        let divLstCats = document.getElementById("divLstCats");
-        showElements(divLstCats, model);
-    }
-    else {
-        document.getElementById("divEmptyList").classList.remove("hidden");
-    }
 
-};
-
+// fonction qui récupère l'élément à valoriser et les datas, 
+// les datas peuvent être sous forme de tableau ou une data unique
+// la fonction vérifie si la data est un tableau, si oui alors elle 
+// boucle sur chacun des éléments en appelant la fonction "showValues"
+// pour valoriser et afficher ces éléments autrement elle appelle la même
+// fonction une fois
 function showElements(el, datas) {
 
     if (datas.length) {
@@ -23,24 +19,43 @@ function showElements(el, datas) {
 
 }
 
+
+// fonction qui clone l'élément à valoriser, récupère les éléments ayant 
+// l'attribut "data-prop", filtre les doublon de valeur "data-prop",
+// puis boucle sur chaques élément récupérés afin de valoriser les éléments
+// où ils doivent l'être (text, name...) en se basant sur la valeur de 
+// l'attribut "data-src", si la valeur de ce dernier est "model", alors on
+// appel la fonction showElements en récursif, et pour finir on injecte l'élément
+// généré dans l'élément parent (ici une div)
 function showValues(el, data) {
 
-    let elClone = el.cloneNode(true);
+    const elClone = el.cloneNode(true);
 
-    let lstElData = elClone.querySelectorAll("[data-prop]");
+    const lstElData = Array.from(elClone.querySelectorAll("[data-prop]"));
 
-    for (let i = 0; i < lstElData.length; i++) {
+    let tabElt = [];
 
-        let currentProp = lstElData[i].getAttribute("data-prop");
+    let lstElDataWithoutDouble = lstElData.filter(el => {
+        if (!tabElt.includes(el.getAttribute("data-prop"))) {
+            tabElt.push(el.getAttribute("data-prop"));
+            return el;
+        }
+    });
+
+    for (let i = 0; i < lstElDataWithoutDouble.length; i++) {
+
+        let currentProp = lstElDataWithoutDouble[i].getAttribute("data-prop");
 
         if (data.hasOwnProperty(currentProp)) {
 
-            let whereInsert = lstElData[i].getAttribute("data-src");
+            let whereInsert = lstElDataWithoutDouble[i].getAttribute("data-src");
 
             switch (whereInsert) {
-                case "text": lstElData[i].textContent = data[currentProp];
+                case "text": lstElDataWithoutDouble[i].textContent = data[currentProp];
                     break;
-                case "name": lstElData[i].setAttribute("name", data[currentProp]);
+                case "name": lstElDataWithoutDouble[i].setAttribute("name", data[currentProp]);
+                    break;
+                case "model": showElements(lstElDataWithoutDouble[i], data[currentProp]);
                     break;
             }
 
@@ -66,11 +81,20 @@ function getFetch(url, fonction) {
         })
 }
 
+function postFetch(url, obj, fonction) {
 
-function showDetails(elt) {
-    let idCat = elt.getAttribute('name');
-    getFetch("Home/catDetails/" + idCat, function (data) {
-        let elt = document.getElementById("divDetailsCat");
-        showElements(elt,data);
-    })
+    fetch("https://localhost:44302/" + url, {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json'
+        }
+        })
+        .then(function (response){
+         return response.json()
+         })
+        .then(function (data) {
+            fonction(data)
+        })
 }
